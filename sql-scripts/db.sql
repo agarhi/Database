04046a7bc4be6ca391b75a7404179bfaa33e35d5
Asif Garhi <asif.garhi@fisglobal.com>
2019-04-25 17:12:17 -0700

    Changed schema

--- sql-scripts/db.sql
+++ sql-scripts/db.sql
@@ -1,7 +1,7 @@
-Create table okddb.user (id INT AUTO_INCREMENT, name varchar(10), PRIMARY KEY (id));
-Create table okddb.phrase (id INT AUTO_INCREMENT, userid int(10), content varchar(100), created DATE, PRIMARY KEY (id), FOREIGN KEY fk_user(userid) REFERENCES user(id));
+Create table okddb.user (userid INT AUTO_INCREMENT, name varchar(10), password varchar(10), PRIMARY KEY (userid));
+Create table okddb.phrase (phraseid INT AUTO_INCREMENT, userid int(10), content varchar(100), created DATE, PRIMARY KEY (phraseid), FOREIGN KEY fk_user(userid) REFERENCES user(userid));
 Create user "test" identified by "test";
 grant select, insert, update, delete on okddb.user to test;
 grant select, insert, update, delete on okddb.phrase to test;
-Insert into user (name) values ("Asif");
+Insert into okddb.user (name,password) values ("Asif","asif");
 
